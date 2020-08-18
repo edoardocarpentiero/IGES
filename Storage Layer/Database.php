ea74ab9a373bd68356db86af3d7dc3607769170c
edoardocarpentiero <edoardo93av@gmail.com>
2020-08-18 17:22:27 +0200

    -Aggiunta documenti di prodotto TER

--- Storage Layer/Database.php
+++ Storage Layer/Database.php
@@ -14,8 +14,8 @@ define("DB_USER", "root");
 define("DB_PASSWORD", "root");
 define("DB_DATABASE", "myprd");
 */
+
 //databaseTesting
-/**/
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
