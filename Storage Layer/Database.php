c9402a3ec182fd8e7b45cbd0326370f68e259462
edoardocarpentiero <edoardo93av@gmail.com>
2020-09-07 19:54:00 +0200

    Refactor class test

--- Storage Layer/Database.php
+++ Storage Layer/Database.php
@@ -15,7 +15,7 @@ define("DB_PASSWORD", "root");
 define("DB_DATABASE", "myprd");
 
 /*
-//databaseTesting
+//databaseTesting TESTING GestioneCaricoDidattico
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
